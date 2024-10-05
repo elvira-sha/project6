759d65c4e4fcb41d4f350b8ed2227e843de00e60
IpHo1 <ilya.cherny2007@gmail.com>
2024-10-05 19:38:05 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int balance = 300;// сумма на счету клиента
+        int xreplenishment = 400; // суммы пополнения
+        int replenishment = 1800; // суммы пополнения
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int bonus;
+        System.out.println("Ваш баланс: " + balance + " рублей ");
+        System.out.println("Поступления на счет: " + xreplenishment + " рублей ");
+        System.out.println("Поступления на счет: " + replenishment + " рублей ");
+
+        if (replenishment >= 1000) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
         }
+        int deposit = balance + xreplenishment + replenishment;
+        System.out.println("Ваш баланс: " + ( deposit + bonus ) + " рублей ");
+        System.out.println("Начисленные бонусы: " + bonus + " бонусных рублей ");
     }
 }
